b895f4ed557323b946a71062b5cc9d97c047c77f
and <andersonvittor1801@gmail.com>
2023-08-29 10:46:25 -0300

    feat(tuplas): aprendendo sobre tuplas em Python

---
+++ Introducao-a-ciencia-de-dados-e-python/5-Estruturas-de-dados/2-Tuplas/02_indices_negativos.py
@@ -0,0 +1,9 @@
+frutas = (
+    "maçâ",
+    "laranja",
+    "uva",
+    "pera",
+)
+
+print(frutas[-1])
+print(frutas[-3])
---
+++ Introducao-a-ciencia-de-dados-e-python/5-Estruturas-de-dados/2-Tuplas/07_index.py
@@ -0,0 +1,4 @@
+linguagens = ("python", "JavaScript", "c", "java", "csharp",)
+
+print(linguagens.index("java"))
+print(linguagens.index("JavaScript"))
