cf9a6772fe4d7cb671d998bae962c2a96b12687c
and <andersonvittor1801@gmail.com>
2023-09-03 16:12:14 -0300

    feat(resolvendo seus primeiros desafios): desafios Python: O grande depósito

---
+++ Resolvendo-seus-primeiros-desafios-de-codigo/5-Desafios Python O Grande Depósito/o_grande_deposito.py
@@ -0,0 +1,12 @@
+valor = float(input("Informe um valor para o depósito: "))
+
+if valor > 0:
+    #TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
+    print("Deposito realizado com sucesso!")
+    print(f"Saldo atual: R$ {valor:.2f}")
+elif valor == 0:
+   #TODO: Imprimir a mensagem de valor inválido.
+   print("Encerrando o programa...")
+else:
+  #TODO: Imprimir a mensagem de encerrar o programa.
+  print("Valor invalido! Digite um valor maior que zero.")
